aa9300dc96ec0d8bdae56089a678e825069e0527
Kevin Diaz <kevin.diaz.perez@hotmail.com>
2015-04-07 19:56:23 -0400

    Update del framework kev

--- index.php
+++ index.php
@@ -1,15 +1,41 @@
 <?php
-    class Mate
-    {
-    	public static function potencia($num1, $num2){
-    		echo pow($num1, $num2);
-    	}
+    /*
 
-    	public static function raiz($num){
-    		echo sqrt($num);
-    	}
+    $text = "holaasdasdasasd,sa";
+    $pos = strpos($text,',');
+    echo substr($text,0,$pos).substr($text, $pos);
+
+    */
+
+    class Conexion{
+        public static function con(){
+            define("host","localhost");
+            define("user","root");
+            define("pass","123456");
+            define("db","framework");
+
+            $con = mysql_connect(host, user, pass);
+            mysql_select_db(db);
+
+            return $con;
+        }
     }
 
-    Mate::raiz(3);
+    class Mysql
+    {
+        public static function ejecutar($var1){
+            return mysql_query($var1, Conexion::con());
+    	}
 
+        public static function arreglo($var1){
+            return mysql_fetch_array($var1);
+        }
+    }
+
+    $sql="select * from users";
+    $res = Mysql::ejecutar($sql);
+
+    while($reg=Mysql::arreglo($res)){
+        echo $reg["nombre"]."<br>";
+    }
 ?>
